b8c567a7b7e9a029720bdd6ccd37530013056708
Robert <rha006@gmail.com>
2024-02-09 19:01:14 +0100

    Added more content and new fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,40 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      rubik: ["Rubik", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+      cooper: ["Cooper", "sans-serif"],
+    },
     extend: {
+      fontSize: {
+        '10xl': ['9rem', '1'],
+        '11xl': ['10rem', '1'],
+        '12xl': ['12rem', '1'],
+      },
       zIndex: {
         'nav-bar': '998',
         'nav-bar-mobile': '999',
-      }
+      },
+      animation: {
+        blob: "blob 20s infinite",
+      },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(70px, -150px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-250px, 100px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [],
