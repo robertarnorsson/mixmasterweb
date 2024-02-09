aefef8a97cea4c4ac78324461d158def9525c6be
Robert <rha006@gmail.com>
2024-02-09 19:12:58 +0100

    Removed background animation because of performance issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,25 +23,6 @@ module.exports = {
         'nav-bar': '998',
         'nav-bar-mobile': '999',
       },
-      animation: {
-        blob: "blob 20s infinite",
-      },
-      keyframes: {
-        blob: {
-          "0%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": {
-            transform: "translate(70px, -150px) scale(1.1)",
-          },
-          "66%": {
-            transform: "translate(-250px, 100px) scale(0.9)",
-          },
-          "100%": {
-            transform: "tranlate(0px, 0px) scale(1)",
-          },
-        },
-      },
     },
   },
   plugins: [],
