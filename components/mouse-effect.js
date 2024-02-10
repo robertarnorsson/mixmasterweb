a4911a700e0eb7287d001a7b66a54323880fd091
Robert <rha006@gmail.com>
2024-02-10 18:25:43 +0100

    Working on the download buttons

--- components/mouse-effect.js
+++ components/mouse-effect.js
@@ -1,13 +1,13 @@
 console.clear();
 
 const circleElement = document.querySelector('.mouse-effect-circle');
+
 const mouse = { x: 0, y: 0 };
-const previousMouse = { x: 0, y: 0 };
+const previousMouse = { x: 0, y: 0 }
 const circle = { x: 0, y: 0 };
 
 let currentScale = 0;
 let currentAngle = 0;
-let targetAngle = 0;
 
 window.addEventListener('mousemove', (e) => {
   mouse.x = e.x;
@@ -27,26 +27,11 @@ window.addEventListener('mousemove', (e) => {
   }
 });
 
-const speed = 0.25;
-
-function normalizeAngle(angle) {
-  angle = angle % 360;
-  if (angle < 0) {
-    angle += 360;
-  }
-  return angle;
-}
-
-function shortestAngleDist(a, b) {
-  const max = 360;
-  const da = (b - a) % max;
-  return 2 * da % max - da;
-}
+const speed = 0.17;
 
 const tick = () => {
   circle.x += (mouse.x - circle.x) * speed;
   circle.y += (mouse.y - circle.y) * speed;
-
   const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
 
   const deltaMouseX = mouse.x - previousMouse.x;
@@ -54,21 +39,15 @@ const tick = () => {
   previousMouse.x = mouse.x;
   previousMouse.y = mouse.y;
 
-  const mouseVelocity = Math.min(Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * 4, 150);
+  const mouseVelocity = Math.min(Math.sqrt(deltaMouseX**2 + deltaMouseY**2) * 4, 150); 
   const scaleValue = (mouseVelocity / 150) * 0.5;
   currentScale += (scaleValue - currentScale) * speed;
-
   const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
 
+  const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
   if (mouseVelocity > 20) {
-    targetAngle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
-    targetAngle = normalizeAngle(targetAngle);
+    currentAngle = angle;
   }
-
-  currentAngle = normalizeAngle(currentAngle);
-  let angleDiff = shortestAngleDist(currentAngle, targetAngle);
-  currentAngle += angleDiff * speed;
-
   const rotateTransform = `rotate(${currentAngle}deg)`;
 
   circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
@@ -77,4 +56,3 @@ const tick = () => {
 }
 
 tick();
-
