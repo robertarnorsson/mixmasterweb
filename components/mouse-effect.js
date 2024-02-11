f3e6fe62bc6647f7e1e5af8c83179a478c5d9b33
Robert <rha006@gmail.com>
2024-02-11 22:01:08 +0100

    Updated mouse effect

--- components/mouse-effect.js
+++ components/mouse-effect.js
@@ -3,54 +3,30 @@ console.clear();
 const circleElement = document.querySelector('.mouse-effect-circle');
 
 const mouse = { x: 0, y: 0 };
-const previousMouse = { x: 0, y: 0 }
 const circle = { x: 0, y: 0 };
-
-let currentScale = 0;
-let currentAngle = 0;
 
 window.addEventListener('mousemove', (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
 
-  if (previousMouse.x === 0 || previousMouse.y === 0) {
-    previousMouse.x = mouse.x
-    previousMouse.y = mouse.y
-    circle.x = mouse.x
-    circle.y = mouse.y
-
-    if ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) {
-      document.querySelector('.mouse-effect-circle').classList.add('hidden');
-    } else {
-      document.querySelector('.mouse-effect-circle').classList.remove('hidden');
-    }
+  // Show or hide the circle on touch devices
+  if ('ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0) {
+    document.querySelector('.mouse-effect-circle').classList.add('hidden');
+  } else {
+    document.querySelector('.mouse-effect-circle').classList.remove('hidden');
   }
 });
 
-const speed = 0.17;
+const speed = 0.15;
 
 const tick = () => {
+  // Update the circle position
   circle.x += (mouse.x - circle.x) * speed;
   circle.y += (mouse.y - circle.y) * speed;
+
+  // Apply the transform to move the circle
   const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
-
-  const deltaMouseX = mouse.x - previousMouse.x;
-  const deltaMouseY = mouse.y - previousMouse.y;
-  previousMouse.x = mouse.x;
-  previousMouse.y = mouse.y;
-
-  const mouseVelocity = Math.min(Math.sqrt(deltaMouseX**2 + deltaMouseY**2) * 4, 150); 
-  const scaleValue = (mouseVelocity / 150) * 0.5;
-  currentScale += (scaleValue - currentScale) * speed;
-  const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
-
-  const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
-  if (mouseVelocity > 20) {
-    currentAngle = angle;
-  }
-  const rotateTransform = `rotate(${currentAngle}deg)`;
-
-  circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
+  circleElement.style.transform = translateTransform;
 
   window.requestAnimationFrame(tick);
 }
